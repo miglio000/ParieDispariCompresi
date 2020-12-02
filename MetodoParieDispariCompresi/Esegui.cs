b67bcaa841dd0fedeb2d4308c5a447e901f10ae4
miglio000 <gabrielemiglio23@gmail.com>
2020-12-02 13:16:20 +0100

    Add project files.

---
+++ MetodoParieDispariCompresi/Esegui.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MetodoParieDispariCompresi
+{
+    public class Esegui
+    {
+        public static uint InserisciInteroPositivo()
+        {
+            Console.WriteLine("Inserisci un numero intero - positivo: ");
+            uint n = uint.Parse(Console.ReadLine());
+            return n;
+        }
+        public static void ContaPariDispari(uint min, uint max)
+        {
+            int c_pari, c_dispari;
+            do
+            {
+                Console.WriteLine("Inserisci il valore minimo: ");
+                min = InserisciInteroPositivo();
+                Console.WriteLine("Inserisci il valore massimo: ");
+                max = InserisciInteroPositivo();
+            } while (min <= max && min<0 && max <0);
+       
+            for(uint i=min; i==max;i++)
+            {
+                int resto = i % 2 == 0;
+                
+            }
+        }
+    }
+}
---
+++ Prova/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MetodoParieDispariCompresi;
+namespace Prova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n1 = Esegui.InserisciInteroPositivo();
+            uint n2 = Esegui.InserisciInteroPositivo();
+            Esegui.ContaPariDispari(n1);
+        }
+    }
+}
